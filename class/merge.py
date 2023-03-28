c66982f0c9226a035d765db253ee70e4e48e7ad2
Bonkyu <rnqhsrb1964@gmail.com>
2023-03-28 17:45:04 +0900

    'sort'

---
+++ class/merge.py
@@ -0,0 +1,37 @@
+def merge_sort(unsorted_list):
+ 
+    if len(unsorted_list) <= 1:
+        return unsorted_list     
+
+    mid = len(unsorted_list)//2
+    left = unsorted_list[:mid]
+    right = unsorted_list[mid:]    
+
+    left_ = merge_sort(left)
+    right_ = merge_sort(right)
+    return merge(left_, right_)
+
+def merge(left, right):
+    i, j = 0,0
+    sorted_list = []
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+    while i < len(left):
+        sorted_list.append(left[i])
+        i += 1
+    while j < len(right):
+        sorted_list.append(right[j])
+        j += 1
+    
+    return sorted_list
+
+
+
+unsorted_list = [7, 3, 8, 6 ,2, 1, 5, 4, 9]
+print(merge_sort(unsorted_list))
